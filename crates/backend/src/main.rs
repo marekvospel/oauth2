5c38462c487df3b7fe56b4e340dbdb4860f0ef15
Marek Vospěl <marek@vospel.cz>
2023-03-03 11:45:20 +0100

    feat: add a protected resource

--- crates/backend/src/main.rs
+++ crates/backend/src/main.rs
@@ -2,6 +2,7 @@
 extern crate rocket;
 extern crate oxide_auth;
 
+pub(self) use crate::routes::*;
 pub(self) use crate::state::Oauth2State;
 
 mod routes;
@@ -10,6 +11,7 @@ mod state;
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/oauth2/", routes![routes::authorize, routes::token])
+        .mount("/oauth2/", routes![oauth2::authorize, oauth2::token])
+        .mount("/", routes![index])
         .manage(Oauth2State::new())
 }
--- crates/backend/src/routes/mod.rs
+++ crates/backend/src/routes/mod.rs
@@ -1,35 +1,26 @@
 use crate::state::Oauth2State;
-use oxide_auth::endpoint::{OwnerConsent, Solicitation};
-use oxide_auth::frontends::simple::endpoint::FnSolicitor;
-use oxide_auth_rocket::{OAuthFailure, OAuthRequest, OAuthResponse};
-use rocket::{Data, State};
+use oxide_auth_rocket::{OAuthFailure, OAuthRequest};
+use rocket::response::Responder;
+use rocket::State;
 
-#[post("/authorize")]
-pub(crate) async fn authorize<'r>(
+pub(crate) mod oauth2;
+
+#[get("/")]
+pub(crate) async fn index<'r>(
     oauth: OAuthRequest<'r>,
     state: &State<Oauth2State>,
-) -> Result<OAuthResponse<'r>, OAuthFailure> {
-    state
+) -> impl Responder<'r, 'r> {
+    let protect = state
         .endpoint()
-        .with_solicitor(FnSolicitor(move |_: &mut _, _: Solicitation<'_>| {
-            // TODO: check whether user is logged in and scopes are valid
-            OwnerConsent::Authorized("marekvospel".into()) as OwnerConsent<OAuthResponse<'r>>
-        }))
-        .authorization_flow()
-        .execute(oauth)
-        .map_err(|err| err.pack::<OAuthFailure>())
-}
+        .with_scopes(vec!["default-scope".parse().unwrap()])
+        .resource_flow()
+        .execute(oauth);
 
-#[post("/token", data = "<body>")]
-pub(crate) async fn token<'r>(
-    mut oauth: OAuthRequest<'r>,
-    body: Data<'r>,
-    state: &State<Oauth2State>,
-) -> Result<OAuthResponse<'r>, OAuthFailure> {
-    oauth.add_body(body).await;
-    state
-        .endpoint()
-        .access_token_flow()
-        .execute(oauth)
-        .map_err(|err| err.pack::<OAuthFailure>())
+    match protect {
+        Ok(grant) => Ok(grant.owner_id),
+        Err(e) => match e {
+            Ok(o) => Err(Ok(o)),
+            Err(e) => Err(Err(e.pack::<OAuthFailure>())),
+        },
+    }
 }
