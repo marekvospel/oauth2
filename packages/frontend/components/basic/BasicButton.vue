82fe0bd8911ca7c56241bc15df87236492b61b90
VIPTrollik <simon@prerovsky.cz>
2023-06-04 23:47:27 +0200

    fix: expand button, lower padding

--- packages/frontend/components/basic/BasicButton.vue
+++ packages/frontend/components/basic/BasicButton.vue
@@ -2,15 +2,26 @@
 interface Props {
   text: string
   type?: 'submit' | 'button'
+  variant?: 'fill' | 'outline'
 }
 
 const props = withDefaults(defineProps<Props>(), {
   type: 'button',
+  variant: 'fill'
 })
+
+const mainStyle = computed(() => {
+  if (props.variant === 'fill') {
+    return 'bg-primary text-black '
+  } else {
+    return 'border-solid border-2 border-primary text-primary'
+  }
+})
+
 </script>
 
 <template>
-  <button :type="type" class="px-5 py-5 bg-primary rounded-md text-black font-semibold text-lg">
+  <button :type="type" :class="mainStyle" class="px-3 py-4 font-semibold variant text-lg rounded-md w-100%">
     {{ text }}
   </button>
 </template>
